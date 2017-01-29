45d4b5e2316809b17c953ffcb6f7359fb3983838
Noah Ezekwugo <nzkwgo@uw.edu>
2017-01-28 17:03:53 -0800

    added link to main.js

--- gameApp/main.js
+++ gameApp/main.js
@@ -1,27 +1,4 @@
 
-//Boilerplate framework from https://github.com/lean/phaser-es6-webpack
-
-import 'pixi';
-import 'p2';
-import Phaser from 'phaser';
-
-import StartState from './states/Start';
-import GameState from './states/Game';
-import LossState from './states/Loss';
-
 //Generates game 
-class Game extends Phaser.Game {
-
-  constructor () {
-
-    super(800, 1200, Phaser.AUTO, "root", null);
-
-    this.state.add('Start', StartState, false);
-    this.state.add('Game', GameState, false);
-    this.state.add('Loss', LossState, false);
-
-    this.state.start('Start');
-  }
-}
-
-var game = new Game();
+alert('hello')
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "game");

aeffdd2198ddaf6ef4a4c1a48ddbb2f408cf6018
leepolla <leepolla@gmail.com>
2017-01-30 14:19:27 -0800

    added firebase dependencies. started experimenting with reading firebase data in descending order

--- gameApp/main.js
+++ gameApp/main.js
@@ -1,5 +1,18 @@
 var game = new Phaser.Game("100%", "100%", Phaser.Auto, 'game');
 
+
+var config = {
+    apiKey: "AIzaSyA39eaG9n2IuM9HPu9s4t5kDtXD-wRpkEo",
+    authDomain: "running-challenge.firebaseapp.com",
+    databaseURL: "https://running-challenge.firebaseio.com",
+    storageBucket: "running-challenge.appspot.com",
+    messagingSenderId: "809639999871"
+};
+firebase.initializeApp(config);
+firebase.auth().signInAnonymously().catch(function(error) {});
+var database = firebase.database();
+var scoresData = database.ref('Scores');
+
 game.state.add("Load", LoadState);
 game.state.add("Menu", MenuState);
 game.state.add("Game", GameState);
--- gameApp/states/Leaderboard.js
+++ gameApp/states/Leaderboard.js
@@ -15,6 +15,69 @@ var LeaderboardState =  {
     var title = game.add.text(game.world.centerX, game.world.centerY - 400, "Leaderboard", titleStyle);
     title.anchor.set(0.5);
 
+    var scoresData = database.ref('Scores');
+    var currentScores = [];
+    var names = [];
+    var scores = [];
+
+    //Function for reading list of firebase data
+    //There are two event listening functions, because we couldnt get either of them to work independently. 
+    //Setting key id guarantees that there isnt duplicate information displayed, even though having two listeners puts duplicate data into our arrays
+    scoresData.orderByChild("ScoreValue").limitToLast(50).on('child_added', function(snapshot) {
+                    var highScore = {UserName: snapshot.val().UserName, ScoreValue: snapshot.val().ScoreValue, keyID: snapshot.key};
+                    names.push(snapshot.val().UserName);
+                    scores.push(snapshot.val().ScoreValue);
+                    currentScores.push(highScore);
+                });        
+
+    // currentScores.sort(function(entry1, entry2) {
+    //     if(entry1.ScoreValue !== entry2.ScoreValue) {
+    //         return entry2.ScoreValue - entry1.ScoreValue;
+    //     } else {
+    //         return entry1.UserName - entry2.UserName;
+    //     }
+    // });
+
+    function scoreCompare(p, q) {
+        if (p < q) {
+          return 1;
+        }
+        if (p > q) {
+          return -1;
+        }
+        return 0;
+    }
+    console.log(currentScores);
+
+
+    currentScores.reverse();
+    console.log(currentScores);
+    currentScores.sort(scoreCompare);
+    console.log(currentScores[0]);
+    function assignRanks(scoreList) {
+        scoreList.sort(function(entry1, entry2) {
+            if(entry1.ScoreValue != entry2.ScoreValue) {
+                return entry2.ScoreValue - entry1.ScoreValue;
+            } else {
+                return entry1.UserName - entry2.UserName;
+            }
+        })
+        var count = 1;
+        scoreList.forEach(function(entry) {
+            entry.rank = count;
+            count++;
+        })
+    }
+    assignRanks(currentScores);
+    console.log(currentScores);
+    console.log(names);
+    console.log(scores);
+    console.log(names.reverse());
+    console.log(scores.reverse());
+    console.log(names[0]);
+    console.log(scores[0]);
+
+
     // var play = game.add.text(game.world.centerX, game.world.centerY, "Play", option);
     // play.anchor.set(0.5);
     // play.inputEnabled = true;
@@ -34,6 +97,10 @@ var LeaderboardState =  {
 
     function goGame() {
         game.state.start('Game');
+    }
+
+    function displayTen(index) {
+
     }
 
   }
